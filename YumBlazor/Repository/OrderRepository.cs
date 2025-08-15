a3a450dc053b0d3f5e2ce68fd6b863d91f5c5c53
Rojen Dangol <rojendangol44@gmail.com>
2025-08-15 12:29:06 +0545

    order record and placement

---
+++ YumBlazor/Repository/OrderRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YumBlazor.Data;
+using YumBlazor.Repository.IRepository;
+
+namespace YumBlazor.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public OrderRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<OrderHeader> CreateAsync(OrderHeader orderHeader)
+        {
+            orderHeader.OrderDate = DateTime.Now;
+            await _db.OrderHeader.AddAsync(orderHeader);
+            await _db.SaveChangesAsync();
+            return orderHeader;
+        }
+
+        public async Task<IEnumerable<OrderHeader>> GetAllAsync(string? userId = null)
+        {
+            if(!string.IsNullOrEmpty(userId))
+            {
+                return await _db.OrderHeader.Where(o => o.UserId == userId).ToListAsync();
+            }
+            return await _db.OrderHeader.ToListAsync();
+        }
+
+        public async Task<OrderHeader> GetAsync(int id)
+        {
+            return _db.OrderHeader.Include(o => o.OrderDetails).FirstOrDefault(o => o.Id == id);
+        }
+
+        public async Task<OrderHeader> UpdateStatusAsync(int orderId, string status)
+        {
+            var orderHeader = _db.OrderHeader.FirstOrDefault(o => o.Id == orderId);
+            if(orderHeader != null)
+            {
+                orderHeader.Status = status;
+                await _db.SaveChangesAsync();
+            }
+            return orderHeader;
+        }
+    }
+}
